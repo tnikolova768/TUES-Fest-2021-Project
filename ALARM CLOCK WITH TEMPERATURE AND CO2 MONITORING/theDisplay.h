b0f740aee86cff5ea43f78c63a87f8fd79e2e0b9
Tsvetelina B. Nikolova <80785238+tnikolova768@users.noreply.github.com>
2021-04-23 08:58:09 +0300

    Delete ALARM CLOCK WITH TEMPERATURE AND CO2 MONITORING directory

--- ALARM CLOCK WITH TEMPERATURE AND CO2 MONITORING/theDisplay.h
+++
@@ -1,8 +0,0 @@
-#if !defined(__THE_CLOCK_THE_DISPLAY_HEADER_INCLUDED_)
-#define __THE_CLOCK_THE_DISPLAY_HEADER_INCLUDED_
-
-extern void theDisplay_init(void);
-extern void theDisplay_process(const unsigned long timestamp);
-
-
-#endif // __THE_CLOCK_THE_DISPLAY_HEADER_INCLUDED_
--- ALARM CLOCK WITH TEMPERATURE AND CO2 MONITORING/theKeys.h
+++
@@ -1,8 +0,0 @@
-#if !defined(__THE_CLOCK_THE_KEYS_HEADER_INCLUDED_)
-#define __THE_CLOCK_THE_KEYS_HEADER_INCLUDED_
-
-extern void theKeys_init(void);
-extern void theKeys_process(const unsigned long timestamp);
-
-
-#endif // __THE_CLOCK_THE_KEYS_HEADER_INCLUDED_
